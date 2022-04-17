05f00506574a72e0a43fb486a4665b00dbf61616
Nico Farr <nicofarr@gmail.com>
2022-04-17 22:24:14 +0000

    finishing touches

--- kmdb.sql
+++ kmdb.sql
@@ -98,6 +98,7 @@
 -- Turns column mode on but headers off
 .mode column
 .headers off
+.width 30 30 30
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 DROP TABLE movies;
